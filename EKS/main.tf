93bc3e416348817d45f6a16445bcdd47a6bebe23
rana <rana@rana.myguest.virtualbox.org>
2024-08-22 14:03:02 +0600

    eks node section changed

--- EKS/main.tf
+++ EKS/main.tf
@@ -35,25 +35,40 @@ module "vpc" {
 
 # EKS
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.24"
+  cluster_name    = "my-cluster"
+  cluster_version = "1.30"
+
 
   cluster_endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  eks_managed_node_groups = {
-    nodes = {
+
+eks_managed_node_groups = {
+    example = {
+      #ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t2.micro"]
+
       min_size     = 1
       max_size     = 3
       desired_size = 2
-
-      instance_type = ["t2.small"]
     }
   }
+
+
+  # eks_managed_node_groups = {
+  #   nodes = {
+  #     min_size     = 1
+  #     max_size     = 3
+  #     desired_size = 2
+
+  #     instance_type = ["t2.micro"]
+  #   }
+  # }
 
   tags = {
     Environment = "dev"
